7c5427e50eb23160f24d021541eadf5406edd729
zeee4real <asadulla.zaid@gmail.com>
2021-03-31 19:03:09 +0530

    - added motion layout animations at start

---
+++ app/src/main/java/com/zayd/architectureimplementation/HomeActivity.kt
@@ -0,0 +1,47 @@
+package com.zayd.architectureimplementation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.card.MaterialCardView
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var view: MaterialCardView
+    private lateinit var home: MotionLayout
+    var clicks = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        home = findViewById(R.id.home)
+        view = findViewById(R.id.view)
+
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        view.setOnClickListener {
+            if (clicks == 0) {
+                setTransition(home.currentState, R.id.end)
+                clicks += 1
+            } else if (clicks == 1) {
+                setTransition(home.currentState, R.id.endnew)
+                clicks += 1
+                view.radius = 0f
+            } else if (clicks == 2) {
+                startActivity(
+                    Intent(
+                        this, MainActivity::class.java
+                    ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                )
+            }
+        }
+    }
+
+    private fun setTransition(startState: Int, endState: Int) {
+        home.setTransition(startState, endState)
+        home.setTransitionDuration(500)
+        home.transitionToEnd()
+    }
+}
